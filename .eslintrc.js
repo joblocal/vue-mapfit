afc71c2f1e565d06bb22b6fb7b984f30a57ecbc1
Roland Olah <rolandolah91+github@gmail.com>
2018-08-03 11:02:46 +0200

    add linter configuration

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+  },
+
+  extends: "airbnb-base",
+
+  env: {
+    browser: true,
+    es6: true,
+  },
+
+  // required to lint *.vue files
+  plugins: [
+    'import',
+    'html',
+  ],
+
+  // check if imports actually resolve
+  settings: {
+    'import/resolver': 'webpack',
+  },
+
+  // add your custom rules here
+  rules: {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never',
+    }],
+
+    // allow debugger/console during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+  }
+};
